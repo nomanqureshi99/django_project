9a1e754bb76e1942fefed2ba1cde8cd1f5c3b30b
nomanqureshi99 <38960042+nomanqureshi99@users.noreply.github.com>
2018-12-22 08:47:44 +0530

    second commit

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,7 @@ PostDeleteView,
 )
 
 urlpatterns = [
-    path('',ListView.as_view(),name='blog-home'),
+    path('',PostListView.as_view(),name='blog-home'),
     path('post/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
     path('post/<int:pk>/update',PostUpdateView.as_view(),name='post-update'),
     path('post/<int:pk>/delete',PostDeleteView.as_view(),name='post-delete'),
